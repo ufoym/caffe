41ee35ba48a5909a4964318ba1b8ec3df3ea6187
Ming <a@ufoym.com>
2015-08-04 20:30:22 +0800

    3rdparty

---
+++ 3rdparty/include/opencv2/opencv_modules.hpp
@@ -0,0 +1,24 @@
+/*
+ *      ** File generated automatically, do not modify **
+ *
+ * This file defines the list of modules available in current build configuration
+ *
+ *
+*/
+
+#define HAVE_OPENCV_CALIB3D
+#define HAVE_OPENCV_CORE
+#define HAVE_OPENCV_FEATURES2D
+#define HAVE_OPENCV_FLANN
+#define HAVE_OPENCV_HAL
+#define HAVE_OPENCV_HIGHGUI
+#define HAVE_OPENCV_IMGCODECS
+#define HAVE_OPENCV_IMGPROC
+#define HAVE_OPENCV_ML
+#define HAVE_OPENCV_SHAPE
+#define HAVE_OPENCV_VIDEO
+#define HAVE_OPENCV_VIDEOIO
+#define HAVE_OPENCV_XFEATURES2D
+#define HAVE_OPENCV_XIMGPROC
+
+
